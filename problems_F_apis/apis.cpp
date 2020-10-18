c934c7ecdfba1b6a7b94d1e4048825ea20075724
Pavel Nazarov <xunterrzn@gmail.com>
2020-10-18 15:57:54 +0100

    problem f apis

--- problems_F_apis/apis.cpp
+++ problems_F_apis/apis.cpp
@@ -1,126 +1,104 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
+#include <string>       // std::string
+#include <sstream>      // std::ostringstream
 #include <math.h>
 #include <sys/stat.h>
 
+#include <curl/curl.h>  
+//#include "nlohmann/json.hpp"
+#include "json.hpp"
+
 bool is_file_exists(char *filename);
 
-struct filetrans {
- int destid;
- int sourcecount;
- int *sourceids = 0;
-};
-
-int N = pow(10, 6);
-int *wiresfrom = 0;
-int *wiresto = 0;
-int Q = pow(10, 9);
-filetrans *transes = 0;
-
-
-bool is_servers_wired(int srcid, int destid, int *path = 0, int pathi = 0);
-
-int main() {
- /*
- FILE *fin = stdin;
- FILE *fout = stdout;
- 
- if (is_file_exists("input.txt")) {
-  fin = fopen("input.txt", "r");
- }
-*/
-
- scanf("%d", &N);
- 
- wiresfrom = (int *)calloc(N, sizeof(int));
- wiresto = (int *)calloc(N, sizeof(int));
- for (int i = 0; i < N; i++) {
-  scanf("%d %d", &wiresfrom[i], &wiresto[i]);
- }
- 
-
- scanf("%d", &Q);
- transes = (filetrans *)malloc(Q * sizeof(filetrans));
- for (int i = 0; i < Q; i++) {
-  filetrans &trans = transes[i];
-  scanf("%d %d", &trans.destid, &trans.sourcecount);
-  trans.sourceids = (int *)malloc(trans.sourcecount * sizeof(int));
-  for (int j = 0; j < trans.sourcecount; j++) {
-   scanf("%d", &trans.sourceids[j]);
-  }
- }
-
- for (int i = 0; i < Q; i++) {
-  filetrans &trans = transes[i];
-  //printf("trans destid=%d sourcecount=%d\n", trans.destid, trans.sourcecount);
-  int avs = 0;
-  int *avids = (int *)calloc(trans.sourcecount, sizeof(int));
-  int avidsi = 0;
-  for (int j = 0; j < trans.sourcecount; j++) {
-   //printf("trans sourceid=%d\n", trans.sourceids[j]);
-   if (is_servers_wired(trans.destid, trans.sourceids[j])) {
-    avs++;
-    avids[avidsi++] = trans.sourceids[j];
-   }
-  }
-  
-  printf("%d", avs);
-  for (int j = 0; j < avidsi; j++) {
-   printf(" %d", avids[j]);
-  }
-  printf("\n");
-
-  free(avids);
- }
- 
- for (int i = 0; i < Q; i++) {
-  free(transes[i].sourceids);
- }
- free(transes);
- free(wiresfrom);
- free(wiresto);
- return 0;
+static size_t curlwritebuf(void *contents, size_t size, size_t nmemb, std::string *sbuf)
+{ 
+ //printf("curlwritebuf\n");
+ size_t realsize = size * nmemb;
+ int bufcurlen = sbuf->length();
+ //printf("sbuf length: %d\n", sbuf->length());
+ sbuf->append((char *)contents, realsize);
+ //printf("sbuf append length: %d\n", sbuf->length());
+ //printf("sbuf append: %s\n", sbuf->c_str());
+ //printf("curlwritebuf: %d\n", realsize);
+ return realsize;
 }
 
-bool is_servers_wired(int srcid, int destid, int *path, int pathi) {
- if (path == 0) {
-  path = (int *)calloc(N, sizeof(int));
- }
- path[pathi] = srcid;
- bool isanyneighboorwired = false;
- for (int i = 0; i < N; i++) {
-  if (wiresfrom[i] == srcid && wiresto[i] == destid || wiresfrom[i] == destid && wiresto[i] == srcid) {
-   isanyneighboorwired = true;
-   break;
-  } 
- }
- if (!isanyneighboorwired) {
-  for (int i = 0; i < N; i++) {   
-   if (wiresfrom[i] == srcid) {	
-    bool circ = false;
-    for (int j = 0; j < pathi + 1; j++) {
-     if (path[j] == wiresto[i]) circ = true;
-    }
-    if (!circ && is_servers_wired(wiresto[i], destid, path, pathi + 1)) {
-     isanyneighboorwired = true;
-     break;
-    }
-   }
-   if (wiresto[i] == srcid) {
-    bool circ = false;
-    for (int j = 0; j < pathi + 1; j++) {
-     if (path[j] == wiresfrom[i]) circ = true;
-    }
-    if (!circ && is_servers_wired(wiresfrom[i], destid, path, pathi + 1)) {
-     isanyneighboorwired = true;
-     break;
-    }
-   }
-  }
+int main() {
+ std::string url;
+ std::getline(std::cin, url);
+
+ int port;
+ scanf("%d", &port);
+
+ int a, b;
+ scanf("%d", &a);
+ scanf("%d", &b);
+
+ if (url[url.length() - 1] == '/') {
+  url.erase(url.length() - 1, 1);
  }
 
- if (pathi == 0) free(path);
- return isanyneighboorwired;
+ std::ostringstream ourls;
+ ourls << url;
+
+ if (port != 80) {
+  //snprintf(url, url.capacity(), ":%d", port);
+  ourls << ':' << port;
+ }
+
+ //snprintf(url, url.capacity(), "/?a=%d&b=%d", a, b);
+ ourls << '/' << "?a=" << a << "&b=" << b;
+
+ url = ourls.str();
+
+ //printf("url: %s\n", url.c_str());
+
+ std::string sbuf;
+
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlwritebuf);
+
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &sbuf);
+ 
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    curl_easy_cleanup(curl);
+
+/*
+*/
+ //printf("sbuf\n");
+ //sbuf = "";
+ //char astr[20] = "1234567890123456789";
+ //printf("astr: %s\n", astr);
+ //curlwritebuf(&astr, sizeof(char), 19, &sbuf);
+
+ //printf("asdfasdf\n");
+ //printf("sbuf: %s\n", sbuf.c_str());
+
+ //sbuf = "[8,6,-2,2,4,17,256,1024,-17,-19]";
+
+ nlohmann::json gotnums = nlohmann::json::parse(sbuf.c_str());
+
+ //printf("json: %s\n", sbuf.c_str());
+ 
+ int acc = 0;
+ for (int i = 0; i < gotnums.size(); i++) {
+  int num = gotnums.at(i);
+  acc += num;
+ }
+ printf("%d\n", acc);
+ return 0;
 }
 
 bool is_file_exists(char *filename) {
